9eff546e43e5c246ebf681b044729115ac42df49
nikmosi <i@nikmosi.ru>
2025-05-13 03:14:19 +0700

    feat(bot): create message reader
    feat(bot): create message reader
    
    chore(README): done read message

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ It's twitch bot. Aim is collect messages from gloria-bot with viewer statistic.
 # TODO
 
 ## Message
+- [x] Read
 - [ ] Collect
 - [ ] Parse
 - [ ] Save
--- main.py
+++ main.py
@@ -1,6 +1,77 @@
-def main():
-    print("Hello from gloria-stalker!")
+from __future__ import annotations
+import asyncio
+from functools import wraps
+from typing import Awaitable, Callable, cast
+from loguru import logger
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.twitch import Twitch
+from twitchAPI.type import AuthScope
+from twitchAPI.chat import Chat, ChatEvent, ChatMessage, EventData
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=[".env.sample", ".env"], env_prefix="gloria_", case_sensitive=False
+    )
+
+    client_id: str = Field(default="...")
+    client_secret: str = Field(default="...")
+    target_chanels: str = "jeensoff"
+    user_scope: list[AuthScope] = [AuthScope.CHAT_READ]
+
+
+def on_ready(target_channel: str) -> Callable[[EventData], Awaitable[None]]:
+    @wraps(on_ready)
+    async def wrapped(ready_event: EventData) -> None:
+        logger.debug("Bot is ready for work, joining channels")
+        await ready_event.chat.join_room(target_channel)
+        logger.info("ready done")
+
+    return wrapped
+
+
+async def on_message(msg: ChatMessage):
+    name = msg.room if msg.room is None else msg.room.name
+    print(f"in {name}, {msg.user.name} said: {msg.text}")
+
+
+async def authenticate(settings: Settings) -> Twitch:
+    logger.debug("Authenticate start")
+    twitch = await Twitch(app_id=settings.client_id, app_secret=settings.client_secret)
+    auth = UserAuthenticator(twitch, settings.user_scope)
+    token, refresh_token = cast(tuple[str, str], await auth.authenticate())
+
+    await twitch.set_user_authentication(token, settings.user_scope, refresh_token)
+    logger.info("Authenticate is complete")
+
+    return twitch
+
+
+async def main():
+    logger.debug("Starting bot")
+    settings = Settings()
+    twitch = await authenticate(settings)
+
+    logger.debug("Creating chat")
+    chat = await Chat(twitch)
+    chat.register_event(ChatEvent.READY, on_ready(settings.target_chanels))
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+    chat.start()
+    logger.info("Chat created")
+
+    try:
+        input("press any key to stop...\n")
+    finally:
+        chat.stop()
+        await twitch.close()
+    logger.info("end")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("got Ctrl-C")
