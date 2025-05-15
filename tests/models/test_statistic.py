c227625ffd216ff12f61f2cdc35a82b99eb8aad4
nikmosi <i@nikmosi.ru>
2025-05-16 02:23:26 +0700

    test(Statistic): add model test
    test(Statistic): add model test
    
    fix(tests): typo in test file name

---
+++ tests/models/test_statistic.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from models.Statistic import Rank, StatisticMessage
+
+
+def test_static_message():
+    date: datetime = datetime.now()
+    nickname: str = "nikmosi"
+    points: int = 10
+    hours: float = 0.1
+    position: int = 20
+    rank: Rank = Rank(name="bob", left=11, right=20)
+
+    message = StatisticMessage(
+        date=date,
+        nickname=nickname,
+        points=points,
+        hours=hours,
+        position=position,
+        rank=rank,
+    )
+
+    assert message.date == date
+    assert message.nickname == nickname
+    assert message.points == points
+    assert message.hours == hours
+    assert message.position == position
+    assert message.rank == rank
+
+
+def test_rank():
+    name = "bob"
+    left = 11
+    right = 20
+
+    rank = Rank(name=name, left=left, right=right)
+
+    assert rank.name == name
+    assert rank.left == left
+    assert rank.right == right
