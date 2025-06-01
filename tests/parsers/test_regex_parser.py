fe0df9c81786cd4a1c835b75b1f7b1328bfa764c
nikmosi <i@nikmosi.ru>
2025-06-02 00:13:32 +0700

    test(RegexParser): add test

---
+++ tests/parsers/test_regex_parser.py
@@ -0,0 +1,24 @@
+import datetime
+
+from models.message import Message
+from parsers.statistic import RegexParser
+
+
+def test_regex_parser():
+    text = (
+        "nikmosi, у вас 329234 очков опыта, провел(а) "
+        "на стримах 2 594,75 часов. В топе 10, ты боб[12/20]."
+    )
+    msg = Message(text=text, date=datetime.datetime.now())
+
+    parser = RegexParser()
+    result = parser.parse(msg)
+
+    assert result is not None
+    assert result.nickname == "nikmosi"
+    assert result.points == 329234
+    assert result.hours == 2594.75
+    assert result.position == 10
+    assert result.rank.name == "боб"
+    assert result.rank.left == 12
+    assert result.rank.right == 20
