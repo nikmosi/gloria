9b34b80df4f84583fa199d2864e405d0820bf808
nikmosi <i@nikmosi.ru>
2025-06-03 04:24:23 +0700

    refactor(main): use authenticate from infra

--- src/main.py
+++ src/main.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 import asyncio
 from collections.abc import Awaitable, Callable
 from functools import wraps
-from typing import cast
 
 from loguru import logger
 from rich.logging import RichHandler
 from twitchAPI.chat import Chat, ChatEvent, ChatMessage, EventData
-from twitchAPI.oauth import UserAuthenticator
-from twitchAPI.twitch import Twitch
 
-from config import Settings, settings
+from config import settings
+from infra.twitch.twitch_auth import authenticate
 
 
 def on_ready(target_channel: str) -> Callable[[EventData], Awaitable[None]]:
@@ -29,18 +27,6 @@ async def on_message(msg: ChatMessage):
     logger.info(
         f"in [bold magenta]{name}[/], [bold yellow]{msg.user.name}[/] said: {msg.text}"
     )
-
-
-async def authenticate(settings: Settings) -> Twitch:
-    logger.debug("Authenticate start")
-    twitch = await Twitch(app_id=settings.client_id, app_secret=settings.client_secret)
-    auth = UserAuthenticator(twitch, settings.user_scope)
-    token, refresh_token = cast(tuple[str, str], await auth.authenticate())
-
-    await twitch.set_user_authentication(token, settings.user_scope, refresh_token)
-    logger.info("Authenticate is complete")
-
-    return twitch
 
 
 async def main():
