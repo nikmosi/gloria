820c3429fe46b249b86779b55396a6e7e0757a77
nikmosi <i@nikmosi.ru>
2025-06-03 04:24:46 +0700

    chore(domain): add MessageSource

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,4 +1,5 @@
 from .message_filter import MessageFilter
 from .message_parser import MessageParser
+from .message_source import MessageSource
 
-__all__ = ["MessageFilter", "MessageParser"]
+__all__ = ["MessageFilter", "MessageParser", "MessageSource"]
