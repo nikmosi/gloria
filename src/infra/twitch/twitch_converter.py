2f65a753bc01374ef356823b08eb35302961acd7
nikmosi <i@nikmosi.ru>
2025-06-03 14:00:21 +0700

    fix(twitch_converter): large sent_timestamp

--- src/infra/twitch/twitch_converter.py
+++ src/infra/twitch/twitch_converter.py
@@ -6,5 +6,5 @@ from domain.models import RawMessage
 
 
 def convert_message(msg: ChatMessage) -> RawMessage:
-    sent_date = datetime.fromtimestamp(msg.sent_timestamp)
-    return RawMessage(text=msg.text, date=sent_date, author=msg.user.name)
+    res = RawMessage(text=msg.text, author=msg.user.name, date=datetime.now())
+    return res
