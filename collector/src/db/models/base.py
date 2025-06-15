91396538353a64ce2ed6785760dd0dbcd8ae2484
nikmosi <i@nikmosi.ru>
2025-06-15 17:00:12 +0700

    chore(db): add typing

--- collector/src/db/models/base.py
+++ collector/src/db/models/base.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Any
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -6,7 +7,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    def to_dict(self):
+    def to_dict(self) -> dict[str, Any]:
         return {c.key: getattr(self, c.key) for c in self.__table__.columns}
 
 
