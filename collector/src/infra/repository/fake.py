2e32f83c58883db3f1312fdb37a51c72336f0b8d
nikmosi <i@nikmosi.ru>
2025-06-19 23:16:08 +0700

    chore(infra/repository): save message in memory

--- collector/src/infra/repository/fake.py
+++ collector/src/infra/repository/fake.py
@@ -8,8 +8,10 @@ from domain.values import ParsedMessage
 
 class FakeRepository(MessageRepository):
     def __init__(self) -> None:
+        self.messages: list[ParsedMessage] = []
         super().__init__()
 
     @override
     async def save(self, msg: ParsedMessage) -> None:
+        self.messages.append(msg)
         logger.debug(msg.model_dump_json())
