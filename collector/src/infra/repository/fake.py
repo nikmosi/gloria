e09e9727159fe4c749edb49fa5752383a5bc981d
nikmosi <i@nikmosi.ru>
2025-06-24 22:02:01 +0700

    chore(repository): make filed private

--- collector/src/infra/repository/fake.py
+++ collector/src/infra/repository/fake.py
@@ -8,10 +8,10 @@ from domain.values import ParsedMessage
 
 class FakeRepository(MessageRepository):
     def __init__(self) -> None:
-        self.messages: list[ParsedMessage] = []
+        self._messages: list[ParsedMessage] = []
         super().__init__()
 
     @override
     async def save(self, msg: ParsedMessage) -> None:
-        self.messages.append(msg)
+        self._messages.append(msg)
         logger.debug(msg.model_dump_json())
